ec91eeedc01bfc2fd27a360949a568a6a7a9400a
Lucas <lucas.s.augusto@hotmail.com>
2025-08-13 11:26:13 -0300

    feat(Users): :sparkles: Add preferences field to userschema

--- apps/users/src/schemas/User.schema.ts
+++ apps/users/src/schemas/User.schema.ts
@@ -2,6 +2,43 @@ import { SchemaFactory, Prop, Schema } from '@nestjs/mongoose';
 import { Credential } from 'apps/auth/src/schemas/Credential.schema';
 import mongoose from 'mongoose';
 
+export enum Themes {
+    LIGHT = 'light',
+    DARK = 'dark',
+    LOFI = 'lofi'
+}
+
+@Schema({
+    _id: false
+})
+export class NotificationPreferences {
+    @Prop({
+        default: true
+    })
+    email: boolean;
+}
+
+@Schema({
+    _id: false
+})
+export class Preferences {
+    @Prop({
+        default: Themes.LIGHT,
+        enum: Object.values(Themes)
+    })
+    theme: string;
+
+    @Prop({
+        default: 'pt-BR'
+    })
+    language: string;
+
+    @Prop({
+        type: NotificationPreferences
+    })
+    notification: NotificationPreferences;
+}
+
 @Schema({
     timestamps: true,
 })
@@ -20,6 +57,11 @@ export class User {
         ref: Credential.name,
     })
     owner: Credential;
+
+    @Prop({
+        type: Preferences
+    })
+    preferences: Preferences;
 }
 
 export const UserSchema = SchemaFactory.createForClass(User);
